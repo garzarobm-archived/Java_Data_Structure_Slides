a1baef6880cde2165ed4e00f54bb4e3a6e88b519
Mike <garzarobm@gmail.com>
2024-04-09 14:29:30 -0500

    Add queue implementation in Prac.java and Queues.java

--- src/etc/Prac.java
+++ src/etc/Prac.java
@@ -1,10 +1,37 @@
 package etc;
 
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Prac {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Queue<Integer> myQueue = new LinkedList<>(); 
 
+		// Adds elements {5, 6, 7, 8, 9} to queue 
+		for (int i=5; i<10; i++) 
+		myQueue.add(i); 
+
+		// Display contents of the queue. 
+		System.out.println("Elements of queue-"+ myQueue); 
+
+		// To remove the head of queue. 
+		int removedHead = myQueue.remove(); 
+		System.out.println("removed element-" + removedHead); 
+
+		System.out.println(myQueue); 
+
+		// To view the head of queue 
+		int head = myQueue.peek(); 
+		System.out.println("head of queue-" + head); 
+
+		// Rest all methods of collection interface, 
+		// Like size and contains can be used with this 
+		// implementation. 
+		int size = myQueue.size(); 
+		System.out.println("Size of queue-" + size); 
 	}
 
 }
--- src/slide_33/Queues.java
+++ src/slide_33/Queues.java
@@ -1,5 +1,24 @@
 package slide_33;
 
-public class Queues {
 
+
+/* BEFORE 
+*
+*
+import java.util.*;
+
+Queue<String> q = new LinkedList<String>();
+*/
+
+//AFTER FIXING
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+	public static void main(String[] args) {
+		Queue<String> q = new LinkedList<String>();
+
+	}
 }
